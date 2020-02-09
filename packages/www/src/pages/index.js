5c3b038f44cad370134c18afe7b314baac9c1e12
Christopher Biscardi <chris@christopherbiscardi.com>
2020-02-08 19:38:29 -0800

    with useEffect

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,21 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, Heading, Button, Flex } from "theme-ui";
 import netlifyIdentity from "netlify-identity-widget";
 
-netlifyIdentity.init({});
-
-export default props => (
-  <Container>
-    <Flex sx={{ flexDirection: "column", padding: 3 }}>
-      <Heading as="h1">Get Stuff Done</Heading>
-      <Button
-        sx={{ marginTop: 2 }}
-        onClick={() => {
-          netlifyIdentity.open();
-        }}
-      >
-        Log In
-      </Button>
-    </Flex>
-  </Container>
-);
+export default props => {
+  useEffect(() => {
+    netlifyIdentity.init({});
+  });
+  return (
+    <Container>
+      <Flex sx={{ flexDirection: "column", padding: 3 }}>
+        <Heading as="h1">Get Stuff Done</Heading>
+        <Button
+          sx={{ marginTop: 2 }}
+          onClick={() => {
+            netlifyIdentity.open();
+          }}
+        >
+          Log In
+        </Button>
+      </Flex>
+    </Container>
+  );
+};
