db36c6518b5cbf130b27b21cd2e3d6e2363bf110
Christopher Biscardi <chris@christopherbiscardi.com>
2020-02-08 23:59:48 -0800

    todos api

--- functions/graphql/graphql.js
+++ functions/graphql/graphql.js
@@ -1,30 +1,55 @@
-const { ApolloServer, gql } = require('apollo-server-lambda');
- 
+const { ApolloServer, gql } = require("apollo-server-lambda");
+
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
   type Query {
-    hello: String
+    todos: [Todo]!
+  }
+  type Todo {
+    id: ID!
+    text: String!
+    done: Boolean!
+  }
+  type Mutation {
+    addTodo(text: String!): Todo
+    updateTodoDone(id: ID!): Todo
   }
 `;
- 
+
+const todos = {};
+let todoIndex = 0;
 // Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
-    hello: () => 'Hello world!',
+    todos: () => {
+      return Object.values(todos);
+    }
   },
+  Mutation: {
+    addTodo: (_, { text }) => {
+      todoIndex++;
+      const id = `key-${todoIndex}`;
+      todos[id] = { id, text, done: false };
+      return todos[id];
+    },
+    updateTodoDone: (_, { id }) => {
+      todos[id].done = true;
+      return todos[id];
+    }
+  }
 };
- 
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  
+
   // By default, the GraphQL Playground interface and GraphQL introspection
   // is disabled in "production" (i.e. when `process.env.NODE_ENV` is `production`).
   //
   // If you'd like to have GraphQL Playground and introspection enabled in production,
   // the `playground` and `introspection` options must be set explicitly to `true`.
   playground: true,
-  introspection: true,
+  introspection: true
 });
- 
+
 exports.handler = server.createHandler();
